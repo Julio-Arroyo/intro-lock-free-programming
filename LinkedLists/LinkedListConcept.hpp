c411b0d8b61eca4cc14bb1aba6e74297a7c72118
Julio-Arroyo <jarroyoi@caltech.edu>
2023-11-08 14:36:49 -0800

    Finished optimisticList. Tested single-threaded.

--- LinkedLists/LinkedListConcept.hpp
+++ LinkedLists/LinkedListConcept.hpp
@@ -2,12 +2,30 @@
 #define LINKED_LIST_CONCEPT_HPP
 
 #include <concepts>
+#include <memory>
+#include <mutex>
+
+template <typename T, typename E>
+concept LinkedListConcept = requires (T t, E e) {
+  { t.add(e)      } -> std::same_as<bool>;
+  { t.remove(e)   } -> std::same_as<bool>;
+  { t.contains(e) } -> std::same_as<bool>;
+};
 
 template <typename T>
-concept LinkedListConcept = requires (T t) {
-  { t.add()      } -> std::same_as<bool>;
-  { t.remove()   } -> std::same_as<bool>;
-  { t.contains() } -> std::same_as<bool>;
+class Node {
+public:
+  Node(const T& v) : val{v}, key{std::hash<T>{}(v)} { }
+
+  // to create sentinels
+  Node(const T& v, const std::size_t k) : val{v}, key{k} { }
+
+std::shared_ptr<Node<T>> next;
+std::mutex mutex;
+std::size_t key;
+
+private:
+  T val;
 };
 
 #endif
--- LinkedLists/OptimisticList.hpp
+++ LinkedLists/OptimisticList.hpp
@@ -1,65 +1,84 @@
 #ifndef OPTIMISTIC_LIST_HPP
 #define OPTIMISTIC_LIST_HPP
 
-#include <mutex>
+#include <iostream>
+#include <cassert>
 #include "LinkedListConcept.hpp"
 
 template <typename T>
 class OptimisticList {
 public:
   OptimisticList() {
-    tail = Node{std::numeric_limits<std::size_t>::max(), std::mutex(), T(), Node{}};
-    head = Node{0, std::mutex(), T(), tail};
+    head = std::make_shared<Node<T>>(T(), 0);
+    tail = std::make_shared<Node<T>>(T(), std::numeric_limits<std::size_t>::max());
+    head->next = tail;
     size = 0;
   }
 
   /* Returns true iff val was not present before adding it */
   bool add(const T& val) {
-    Node pred = head;
-    Node curr = head.next;
+    std::shared_ptr<Node<T>> pred = head;
+    std::shared_ptr<Node<T>> curr = head->next;
     std::size_t newKey = std::hash<T>{}(val);
 
     while (true) {  // loop repeatedly until validate() succeeds
-      while (curr.key < newKey) {  // loop until pred.key < newKey <= curr.key
+      while (curr->key < newKey) {  // loop until pred->key < newKey <= curr->key
         pred = curr;
-        curr = curr.next;
+        curr = curr->next;
       }
 
-      std::lock_guard<std::mutex> predLock(pred.lock);
-      std::lock_guard<std::mutex> currLock(curr.lock);
+      std::lock_guard<std::mutex> predLock(pred->mutex);
+      std::lock_guard<std::mutex> currLock(curr->mutex);
       if (validate(pred, curr)) {
-        if (curr.key != newKey) {  // add newNode if not present
-          Node newNode = Node{newKey, std::mutex(), val, curr};
-          pred.next = newNode;
+        if (curr->key != newKey) {  // add newNode if not present
+          Node dn = Node(val);
+          std::shared_ptr<Node<T>> newNode = std::make_shared<Node<T>>(val);
+          newNode->next = curr;
+          pred->next = newNode;
           size++;
+
+          #ifdef DEBUG_LOGGING
+            std::cout << "ADD," << val << "," << true << std::endl;
+          #endif
           return true;
         }
+
+        #ifdef DEBUG_LOGGING
+          std::cout << "ADD," << val << "," << false << std::endl;
+        #endif
         return false;
       }
     }
-    assert(false, "ERROR: unreachable");
+    assert(false);  // unreachable
   } 
 
   /* Returns true if val was removed from the set, false if it was not present */
   bool remove(const T& val) {
-    Node pred = head;
-    Node curr = head.next;
+    std::shared_ptr<Node<T>> pred = head;
+    std::shared_ptr<Node<T>> curr = head->next;
     std::size_t removedKey = std::hash<T>{}(val);
 
     while (true) {
-      while (curr.key < removedKey) {
+      while (curr->key < removedKey) {
         pred = curr;
-        cur = curr.next;
+        curr = curr->next;
       }
 
-      std::lock_guard<std::mutex> predLock(pred.lock);
-      std::lock_guard<std::mutex> currLock(curr.lock);
+      std::lock_guard<std::mutex> predLock(pred->mutex);
+      std::lock_guard<std::mutex> currLock(curr->mutex);
       if (validate(pred, curr)) {
-        if (curr.key == removedKey) {
-          pred.next = curr.next;
+        if (curr->key == removedKey) {
+          pred->next = curr->next;
           size--;
+
+          #ifdef DEBUG_LOGGING
+            std::cout << "REM," << val << "," << true << std::endl;
+          #endif
           return true;
         } else {
+          #ifdef DEBUG_LOGGING
+            std::cout << "REM," << val << "," << false << std::endl;
+          #endif
           return false;
         }
       }
@@ -67,49 +86,46 @@ public:
   }
 
   bool contains(const T& val) {
-    Node pred = head;
-    Node curr = head.next;
+    std::shared_ptr<Node<T>> pred = head;
+    std::shared_ptr<Node<T>> curr = head->next;
     std::size_t searchKey = std::hash<T>{}(val);
 
     while (true) {
-      while (curr.key < searchKey) {
+      while (curr->key < searchKey) {
         pred = curr;
-        cur = curr.next;
+        curr = curr->next;
       }
 
-      std::lock_guard<std::mutex> predLock(pred.lock);
-      std::lock_guard<std::mutex> currLock(curr.lock);
+      std::lock_guard<std::mutex> predLock(pred->mutex);
+      std::lock_guard<std::mutex> currLock(curr->mutex);
       if (validate(pred, curr)) {
-        return curr.key == searchKey;
+        #ifdef DEBUG_LOGGING
+          std::cout << "CON," << val << "," << (curr->key == searchKey) << std::endl;
+        #endif
+        return curr->key == searchKey;
       }
     }
   }
 
 private:
-  bool validate(const Node& pred, const Node& curr) {
-    Node node = head;
+  /* Returns true if pred is reachable AND points to curr */
+  bool validate(const std::shared_ptr<Node<T>> pred, const std::shared_ptr<Node<T>> curr) {
+    std::shared_ptr<Node<T>> node = head;
     while (true) {
-      if (node.key > pred.key) {
+      if (node->key > pred->key) {
         return false;  // pred not found
-      } else if (node.key == pred.key) {
-        return pred.next.key = curr.key;
+      } else if (node->key == pred->key) {
+        return pred->next->key = curr->key;
       }
-      node = node.next;
+      node = node->next;
     }
   }
 
-  struct Node {
-    std::size_t key;
-    std::mutex mutex;
-    T val;
-    Node next;
-  }
-
-  Node head;
-  Node tail;
+  std::shared_ptr<Node<T>> head;
+  std::shared_ptr<Node<T>> tail;
   std::size_t size;
 };
-static_assert(LinkedListConcept<OptimisticList<int>>);  // TODO: can 'int' be made arbitrary?
+static_assert(LinkedListConcept<OptimisticList<int>, int>);  // TODO: can 'int' be made arbitrary?
 
 #endif
 
